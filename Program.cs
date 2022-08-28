2c43e3a73884c315bfb6c709aad681009dad26df
Gilson Caetao <gilsonebcs@gmail.com>
2022-08-28 11:25:22 -0300

    Projeto em Liguagem C# Compesa Abastecer com Gasolina / Álcoon

---
+++ Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+
+class Program {
+  
+	public static void Main (string[] args) {
+		double gasolina,etanol;
+		string decisao = "";
+		string texto;
+
+	double posto= 0.7;
+    string resultado;
+	
+    Console.Write("Digite o Usúario: ");
+    string nome = Console.ReadLine();
+	Console.Clear();
+
+	while (decisao != "s" && decisao != "S")
+		{
+    texto = "Sejá Bem vindo: " + (nome);
+	texto = texto.ToUpper();
+	Console.WriteLine(texto);
+	
+    Console.Write("Qual o valor da Gasolina: ");
+    gasolina = double.Parse(Console.ReadLine());
+    
+    Console.Write("Qual o valor do Etanol: ");
+    etanol = double.Parse(Console.ReadLine());
+   
+	double gas = 0.7 * gasolina;
+    double eta = 0.7 * etanol;
+	
+    Console.WriteLine("Gasolina: {0:N2}",  gas );
+    Console.WriteLine("Etanol: {0:N2}",  eta );
+
+	posto=gasolina+etanol;
+
+	 if((gasolina * 0.7) == eta){
+       resultado="Tanto faz Gasolina ou Etanol. ";
+    }else if((gasolina * 0.7) > eta){
+		resultado="Compensa abastecer com Etanol.";
+	}else{
+		resultado="Compensa abastecer com Gasolina.";
+	}
+		
+		Console.WriteLine("Resultado: "+ resultado);
+	
+		Console.Write("Deseja Encerrar Pressione S / N: ");
+		decisao = Console.ReadLine();
+		Console.Clear();
+
+			}
+		
+		}
+		
+	}
